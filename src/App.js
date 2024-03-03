f3b24a9e6a32b37e3a434212093dee48d8ab52a6
Bidhun V K <bidhunvaikkath@gmail.com>
2024-03-03 06:31:50 +0530

    Complete design with some static data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar.js";
+import './App.css'
+import Banner from "./Components/Banner/Banner.js";
+import RowPost from "./Components/RowPost/RowPost.js";
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Banner />
+      <RowPost/>
     </div>
   );
 }
